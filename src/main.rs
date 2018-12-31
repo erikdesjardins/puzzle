c48fd8f537d61d3c3bacd183a2a9ebd020d2a873
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2018-12-31 14:27:19 -0500

    implement conversion from enums to state, reverse byte order

--- src/main.rs
+++ src/main.rs
@@ -23,19 +23,19 @@ fn swap_bytes(x: u128, i: usize, j: usize) -> u128 {
 }
 
 fn valid(state: u128) -> bool {
-    // a1 b1 c1 d1 a2 b2 c2 d2 a3 b3 c3 d3 a4 b4 c4 d4
-    let succeeding_rows = state;
-    //             a1 b1 c1 d1 a2 b2 c2 d2 a3 b3 c3 d3
-    let preceeding_rows = swap_nibbles(state >> (4 * 8));
+    // d4 c4 b4 a4 d3 c3 b3 a3 d2 c2 b2 a2 d1 c1 b1 a1
+    let preceeding_rows = state;
+    //             d4 c4 b4 a4 d3 c3 b3 a3 d2 c2 b2 a2
+    let succeeding_rows = state >> (4 * 8);
     // compare each row with the following row, ignoring wraparound comparisons
-    let rows_match = all_bytes_nonzero((preceeding_rows & succeeding_rows) | 0xffffffff000000000000000000000000);
+    let rows_match = all_bytes_nonzero((preceeding_rows & swap_nibbles(succeeding_rows)) | 0xffffffff000000000000000000000000);
 
-    // a1 b1 c1 d1 a2 b2 c2 d2 a3 b3 c3 d3 a4 b4 c4 d4
-    let succeeding_cols = state;
-    //    a1 b1 c1 xx a2 b2 c2 xx a3 b3 c3 xx a4 b4 c4
-    let preceeding_cols = swap_nibbles(state >> (1 * 8));
+    // d4 c4 b4 a4 d3 c3 b3 a3 d2 c2 b2 a2 d1 c1 b1 a1
+    let preceeding_cols = state;
+    //    d4 c4 b4 xx d3 c3 b3 xx d2 c2 b2 xx d1 c1 b1
+    let succeeding_cols = state >> (1 * 8);
     // compare each col with the following col, ignoring cross-row comparisons
-    let cols_match = all_bytes_nonzero((preceeding_cols & succeeding_cols) | 0xff000000ff000000ff000000ff000000);
+    let cols_match = all_bytes_nonzero((preceeding_cols & swap_nibbles(succeeding_cols)) | 0xff000000ff000000ff000000ff000000);
 
     // TODO try using a branch in between cols/rows and see if it's faster
     // TODO it will probably get threaded into the loop branch anyways
@@ -80,21 +80,59 @@ fn check_all_permutations_of(state: u128) {
 #[cold]
 #[inline(never)]
 fn print(mut state: u128) {
-    let mut pull_high_byte = move || {
-        let high = (state >> (128 - 8)) as u8;
-        state <<= 8;
-        high
+    let mut pull_low_byte = move || {
+        let low = state as u8;
+        state >>= 8;
+        low
     };
 
     for _ in 0..4 {
         for _ in 0..4 {
-            print!("{:0>2x} ", pull_high_byte());
+            print!("{:0>2x} ", pull_low_byte());
         }
         println!();
     }
 }
 
+#[derive(Copy, Clone)]
+#[repr(u8)]
+enum Hole {
+    HO /* ctagon */ = 1 << 7,
+    HC /* ross */ = 1 << 6,
+    HI /* n arrow */ = 1 << 5,
+    HA /* rrow */ = 1 << 4,
+}
+use self::Hole::*;
+
+#[derive(Copy, Clone)]
+#[repr(u8)]
+// note In/Out swapped, since from the perspective of each tile the opposite ones fit together
+enum Prod {
+    PO /* ctagon */ = 1 << 3,
+    PC /* ross */ = 1 << 2,
+    PI /* n arrow */ = 1 << 0,
+    PA /* rrow */ = 1 << 1,
+}
+use self::Prod::*;
+
+fn to_state(tiles: [[(Hole, Hole, Prod, Prod); 4]; 4]) -> u128 {
+    let mut state = 0;
+    for (i, row) in tiles.iter().enumerate() {
+        for (j, &(h1, h2, p1, p2 )) in row.iter().enumerate() {
+            let byte = h1 as u8 | h2 as u8 | p1 as u8 | p2 as u8;
+            state |= (byte as u128) << ((i * 4 + j) * 8);
+        }
+    }
+    state
+}
+
 fn main() {
-    let state = 0x7fb8670bc88b6743d115534466e2f4fd;
+    let tiles = [
+        [(HO, HO, PO, PO), (HO, HO, PO, PC), (HO, HO, PO, PO), (HO, HO, PO, PO)],
+        [(HO, HO, PC, PC), (HC, HA, PI, PI), (HO, HO, PO, PO), (HO, HO, PO, PO)],
+        [(HO, HO, PO, PO), (HO, HO, PO, PO), (HO, HO, PO, PO), (HO, HO, PO, PO)],
+        [(HO, HO, PO, PO), (HO, HO, PO, PO), (HO, HO, PO, PO), (HO, HO, PO, PO)],
+    ];
+    let state = to_state(tiles);
     check_all_permutations_of(state);
 }
