db578e0938210adfef03108ae95b7a28a53203eb
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2019-01-11 00:42:25 -0500

    working impl (simple tree search)
    finds all (non-flip) solutions in less than a second...
    this is a lesson in not over-estimating complexity
    although there are a _lot_ of configurations in theory, huge branches of the tree get pruned, so tree search is very tractable, no advanced optimization or constraint solving necessary.

--- src/main.rs
+++ src/main.rs
@@ -1,165 +1,162 @@
-mod u128;
+use std::marker::PhantomData;
 
-use std::mem::size_of;
+use self::Edge::*;
 
-use crate::u128::Ext;
-
-fn valid(state: u128) -> bool {
-    // d4 c4 b4 a4 d3 c3 b3 a3 d2 c2 b2 a2 d1 c1 b1 a1
-    let preceeding_rows = state;
-    //             d4 c4 b4 a4 d3 c3 b3 a3 d2 c2 b2 a2
-    let succeeding_rows = state >> (4 * 8);
-    // compare each row with the following row, ignoring wraparound comparisons
-    let rows_match = ((preceeding_rows & succeeding_rows.swap_nibbles()) | 0xffffffff000000000000000000000000).all_bytes_nonzero();
-    if !rows_match { return false }
-
-    // d4 c4 b4 a4 d3 c3 b3 a3 d2 c2 b2 a2 d1 c1 b1 a1
-    let preceeding_cols = state;
-    //    d4 c4 b4 xx d3 c3 b3 xx d2 c2 b2 xx d1 c1 b1
-    let succeeding_cols = state >> (1 * 8);
-    // compare each col with the following col, ignoring cross-row comparisons
-    let cols_match = ((preceeding_cols & succeeding_cols.swap_nibbles()) | 0xff000000ff000000ff000000ff000000).all_bytes_nonzero();
-    if !cols_match { return false }
-
-    true
+#[derive(Copy, Clone)]
+#[repr(u8)]
+enum Edge {
+    // Holes
+    HO /* ctagon */ = 0 & 0b1111,
+    HC /* ross */ = 1 & 0b1111,
+    HI /* n arrow */ = 2 & 0b1111,
+    HA /* rrow */ = 3 & 0b1111,
+    // Prods
+    // Each prod is the complement of the matching hole
+    // Note: In/Out swapped, since from the perspective of each tile the opposite ones fit together
+    PO /* ctagon */ = !0 & 0b1111,
+    PC /* ross */ = !1 & 0b1111,
+    PI /* n arrow */ = !3 & 0b1111,
+    PA /* rrow */ = !2 & 0b1111,
 }
 
-// todo perhaps use a "naive" search, recursing 16 times and picking the only pieces that fit
-//  to opt, use typelevel numbers to terminate recursion (if necessary) and force inline
-fn all_permutations_of(original_state: u128, mut output: impl FnMut(u128)) {
-    const N: usize = size_of::<u128>();
-    let is_even = |x| x % 2 == 0;
-
-    let mut state = original_state;
-    let mut c = [0; N];
-    let mut i = 0;
-
-    output(state);
-
-    while i < N {
-        if c[i] < i {
-            if is_even(i) {
-                state = state.swap(0, i);
-            } else {
-                state = state.swap(c[i], i);
-            }
-            output(state);
-            c[i] += 1;
-            i = 0;
-        } else {
-            c[i] = 0;
-            i += 1;
-        }
+fn nibble_to_edge(x: u8) -> Edge {
+    let x = x & 0b1111;
+    match () {
+        _ if x == HO as u8 => HO,
+        _ if x == HC as u8 => HC,
+        _ if x == HI as u8 => HI,
+        _ if x == HA as u8 => HA,
+        _ if x == PO as u8 => PO,
+        _ if x == PC as u8 => PC,
+        _ if x == PI as u8 => PI,
+        _ if x == PA as u8 => PA,
+        _ => unreachable!(),
     }
 }
 
-// normal
-#[cfg(all())]
-fn check_all_permutations_of(state: u128) {
-    all_permutations_of(state, |state| {
-        if valid(state) {
-            print(state);
-        }
-    })
-}
+/// Elements in row-major order:
+///  00 01 02 03
+///  04 05 06 07
+///  08 09 10 11
+///  12 13 14 15
+///
+/// Sides clockwise from the top:
+///  (a, b, c, d)
+/// OR
+///  0b dddd cccc bbbb aaaa
+///  ┌ a ┐
+///  d   b
+///  └ c ┘
+type Tiles = [[(Edge, Edge, Edge, Edge); 4]; 4];
+type State = [u16; 16];
+/// Per-side right shifts
+const TOP: u8 = 0 * 4;
+const RIGHT: u8 = 1 * 4;
+const BOTTOM: u8 = 2 * 4;
+const LEFT: u8 = 3 * 4;
 
-// for vtune
-#[cfg(any())]
-fn check_all_permutations_of(state: u128) {
-    // include calls to this to avoid dce
-    #[cold]
-    #[inline(never)]
-    fn sink(x: u128) {
-        std::sync::atomic::compiler_fence(std::sync::atomic::Ordering::SeqCst);
+fn tiles_to_state(tiles: Tiles) -> State {
+    let mut state = [0; 16];
+    for (i, row) in tiles.iter().enumerate() {
+        for (j, &(a, b, c, d )) in row.iter().enumerate() {
+            state[i * 4 + j] |= (a as u16) << TOP;
+            state[i * 4 + j] |= (b as u16) << RIGHT;
+            state[i * 4 + j] |= (c as u16) << BOTTOM;
+            state[i * 4 + j] |= (d as u16) << LEFT;
+        }
     }
-    all_permutations_of(state, |state| {
-        if valid(state) {
-            sink(state);
-        }
-    });
+    state
 }
 
-// for time profiling
-#[cfg(any())]
-fn check_all_permutations_of(state: u128) {
-    use std::time::Instant;
-    let mut i = 0;
-    let mut count = 0;
-    let start = Instant::now();
-    #[cold]
-    #[inline(never)]
-    fn end(start: Instant, valid: u32, all: u32) {
-        let elapsed = start.elapsed();
-        let secs = (elapsed.as_secs() as f64) + (elapsed.subsec_nanos() as f64) / (1_000_000_000 as f64);
-        let nanos = (elapsed.as_secs() as u128 * 1_000_000_000) + elapsed.subsec_nanos() as u128;
-        println!("{:0.1} s, {} ns/1, {:0.0} M/s", secs, nanos / all as u128, all as f64 / secs / 1_000_000.);
-        println!("{:0.1} %", valid as f64 / all as f64 * 100.);
-        panic!();
+fn state_to_tiles(state: State) -> Tiles {
+    let mut tiles = [[(HO, HO, HO, HO); 4]; 4];
+    for (i, row) in tiles.iter_mut().enumerate() {
+        for (j, (a, b, c, d)) in row.iter_mut().enumerate() {
+            *a = nibble_to_edge((state[i * 4 + j] >> TOP) as u8);
+            *b = nibble_to_edge((state[i * 4 + j] >> RIGHT) as u8);
+            *c = nibble_to_edge((state[i * 4 + j] >> BOTTOM) as u8);
+            *d = nibble_to_edge((state[i * 4 + j] >> LEFT) as u8);
+        }
     }
-    all_permutations_of(state, |state| { i += 1;
-        if i == 1_000_000_000 {
-            end(start, count, i);
-        }
-        if valid(state) { count += 1; }
-    })
+    tiles
 }
 
-
-#[cold]
-#[inline(never)]
-fn print(mut state: u128) {
-    let mut pull_2_bits = move || {
-        let low = state as u8;
-        state >>= 2;
-        low & 0b11
-    };
-
-    for _row in 0..4 {
-        for _col in 0..4 {
-            for _side in 0..4 {
-                print!("{}", pull_2_bits());
-            }
-            print!(" ");
+fn print(tiles: Tiles) {
+    fn edge_to_str(edge: Edge) -> &'static str {
+        match edge {
+            HO => "(o)",
+            HC => "(†)",
+            HI => "(A)",
+            HA => "(I)",
+            PO => " o ",
+            PC => " † ",
+            PI => " I ",
+            PA => " A ",
         }
+    }
+    for row in tiles.iter() {
+        for &(a, _, _, _) in row { print!(" ┌{}┐ ", edge_to_str(a)); }
+        println!();
+        for &(_, b, _, d) in row { print!("{} {}", edge_to_str(d), edge_to_str(b)); }
+        println!();
+        for &(_, _, c, _) in row { print!(" └{}┘ ", edge_to_str(c)); }
         println!();
     }
     println!();
 }
 
-#[derive(Copy, Clone)]
-#[repr(u8)]
-enum Hole {
-    HO /* ctagon */ = 0,
-    HC /* ross */ = 1,
-    HI /* n arrow */ = 2,
-    HA /* rrow */ = 3,
-}
-use self::Hole::*;
-
-#[derive(Copy, Clone)]
-#[repr(u8)]
-// note In/Out swapped, since from the perspective of each tile the opposite ones fit together
-enum Prod {
-    PO /* ctagon */ = 0,
-    PC /* ross */ = 1,
-    PI /* n arrow */ = 3,
-    PA /* rrow */ = 2,
-}
-use self::Prod::*;
-
-fn to_state(tiles: [[(Hole, Hole, Prod, Prod); 4]; 4]) -> u128 {
-    let mut state = 0;
-    for (i, row) in tiles.iter().enumerate() {
-        for (j, &(h1, h2, p1, p2 )) in row.iter().enumerate() {
-            let byte =
-                ((h1 as u8) << (0 * 2)) |
-                ((h2 as u8) << (1 * 2)) |
-                ((p1 as u8) << (2 * 2)) |
-                ((p2 as u8) << (3 * 2));
-            state |= (byte as u128) << ((i * 4 + j) * 8);
+fn find_solutions(state: State) {
+    // recursion with typelevel bounds to ensure all recursive calls can be inlined
+    struct Z;
+    struct S<T>(PhantomData<T>);
+    trait Recur {
+        const INDEX: usize;
+        fn run(state: State);
+    }
+    impl<T: Recur> Recur for S<T> {
+        const INDEX: usize = T::INDEX - 1;
+        fn run(mut state: State) {
+            let i = Self::INDEX;
+            // try swapping with all future indices,
+            // and the current index (i.e. keeping it in place)
+            for j in i..16 {
+                for rot in 0..4 {
+                    // piece to be swapped into the current index
+                    let j_piece = state[j].rotate_right(rot * 4);
+                    // check index immediately before and above
+                    let is_first_col = (i % 4) == 0;
+                    let before_valid = is_first_col || {
+                        let before = i - 1;
+                        (state[before] >> RIGHT) & 0b1111 == !(j_piece >> LEFT) & 0b1111
+                    };
+                    let is_first_row = i < 4;
+                    let above_valid = is_first_row || {
+                        let above = i - 4;
+                        (state[above] >> BOTTOM) & 0b1111 == !(j_piece >> TOP) & 0b1111
+                    };
+                    if before_valid && above_valid {
+                        // todo maybe pass down state as mut ref and reverse the swap after
+                        state[j] = state[i];
+                        state[i] = j_piece;
+                        T::run(state);
+                    }
+                }
+            }
         }
     }
-    state
+    impl Recur for Z {
+        const INDEX: usize = 16;
+        #[cold]
+        #[inline(never)]
+        fn run(state: State) {
+            // this point is only reached if all indices are valid
+            print(state_to_tiles(state));
+        }
+    }
+
+    type S15 = S<S<S<S<S<S<S<S<S<S<S<S<S<S<S<S<Z>>>>>>>>>>>>>>>>;
+    assert_eq!(S15::INDEX, 0);
+    S15::run(state);
 }
 
 fn main() {
@@ -171,6 +168,5 @@ fn main() {
         [(HO, HI, PO, PO), (HI, HO, PI, PA), (HI, HA, PI, PC), (HC, HA, PO, PI)],
         [(HA, HO, PO, PC), (HO, HC, PA, PO), (HO, HC, PI, PI), (HC, HA, PC, PA)],
     ];
-    let state = to_state(tiles);
-    check_all_permutations_of(state);
+    find_solutions(tiles_to_state(tiles));
 }
